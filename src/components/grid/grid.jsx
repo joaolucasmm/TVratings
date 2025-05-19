4ced2ed245fd5b86cefe13f4b1a83151815ad0da
Caroline Savi <carolinesavii@gmail.com>
2025-05-19 15:18:31 -0300

    Delete const from grid
    for better reading the variable wasdelete ...

--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -9,77 +9,76 @@ import Paper from "@mui/material/Paper";
 import supabase from "../../config/supabaseClient";
 
 export default function Grid() {
-    const [fetchError, setFetchError] = useState(null);
-    const [shows, setShows] = useState(null);
+  const [fetchError, setFetchError] = useState(null);
 
-    useEffect(() => {
-        const fetchShows = async () => {
-            const { data, error } = await supabase.from("shows").select("*");
-            
-            if (error) {
-                setFetchError("Could not fetch the data");
-                setShows(null);
-            }
-            if (data) {
-                setShows(data);
-                setFetchError(null);
-            }
-        };
-        fetchShows();
-    }, []);
+  useEffect(() => {
+    const fetchShows = async () => {
+      const { data, error } = await supabase.from("shows").select("*");
 
-    function createData(title, duration, genre, rating, release) {
-        return { title, duration, genre, rating, release };
-    }
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setShows(null);
+      }
+      if (data) {
+        setShows(data);
+        setFetchError(null);
+      }
+    };
+    fetchShows();
+  }, []);
 
-    const rows = shows
-        ? shows.map((show) =>
-              createData(show.title, show.duration, show.genre, show.rating, show.release)
-          )
-        : [];
+  function createData(title, duration, genre, rating, release) {
+    return { title, duration, genre, rating, release };
+  }
 
-    return (
-        <>
-            {fetchError && <div>{fetchError}</div>}
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Title</TableCell>
-                            <TableCell align="right">Duration</TableCell>
-                            <TableCell align="right">Genre</TableCell>
-                            <TableCell align="right">Rating</TableCell>
-                            <TableCell align="right">Release</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.title}
-                                sx={{
-                                    "&:last-child td, &:last-child th": {
-                                        border: 0,
-                                    },
-                                }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.title}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.duration}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.genre}
-                                </TableCell>
-                                <TableCell align="right">{row.rating}</TableCell>
-                                <TableCell align="right">
-                                    {row.release}
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-    );
+  const rows = shows
+    ? shows.map((show) =>
+        createData(
+          show.title,
+          show.duration,
+          show.genre,
+          show.rating,
+          show.release
+        )
+      )
+    : [];
+
+  return (
+    <>
+      {fetchError && <div>{fetchError}</div>}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Duration</TableCell>
+              <TableCell align="right">Genre</TableCell>
+              <TableCell align="right">Rating</TableCell>
+              <TableCell align="right">Release</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.title}
+                sx={{
+                  "&:last-child td, &:last-child th": {
+                    border: 0,
+                  },
+                }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.title}
+                </TableCell>
+                <TableCell align="right">{row.duration}</TableCell>
+                <TableCell align="right">{row.genre}</TableCell>
+                <TableCell align="right">{row.rating}</TableCell>
+                <TableCell align="right">{row.release}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
