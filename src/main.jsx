0cb22f2183de8f25edac462c9158e8ec87fa91fc
joaolucasmm <joaol_mm@hotmail.com>
2025-06-03 16:23:53 -0300

    Create routes to each page and header menu

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import About from './About.jsx'
-import NotFound from './NotFound.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import About from "./About.jsx";
+import NotFound from "./NotFound.jsx";
+import ResponsiveAppBar from "./components/appbar/Appbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/about', element: <About />},
-  { path: '*', element: <NotFound /> }
+    {
+        path: "/",
+        element: (
+            <>
+                <ResponsiveAppBar />
+                <App />
+            </>
+        ),
+    },
+    {
+        path: "/about",
+        element: (
+            <>
+                <ResponsiveAppBar />
+                <About />
+            </>
+        ),
+    },
+    {
+        path: "*",
+        element: (
+            <>
+                <ResponsiveAppBar />
+                <NotFound />
+            </>
+        ),
+    },
 ]);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
